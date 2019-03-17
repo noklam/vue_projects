3d9ec50f6b983046fb588606b9f38e5b0c4ed363
noklam <channoklam@yahoo.com.hk>
2019-03-17 17:11:47 +0800

    Add menu drawer

--- hello-world/src/components/Navbar.vue
+++ hello-world/src/components/Navbar.vue
@@ -1,12 +1,31 @@
 <template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Fake</span>
+        <span>Data Scientist</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
 
-    <nav>Hello</nav>
-
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
 </script>
 
--- hello-world/src/views/Home.vue
+++ hello-world/src/views/Home.vue
@@ -1,20 +1,24 @@
 <template>
-<div class="home">
-  <h1 class="display-2">This is a page of a Fake Data Scientist</h1>
- <v-btn class="pink"></v-btn>
- <v-btn color="pink">Click me</v-btn>
-</div>
- 
+  <div class="home">
+    <h1 class="display-2 grey--text">This is a page of a Fake Data Scientist</h1>
+    <v-btn depressed class="teal accent-4 white--text">
+      <v-icon left medium>email</v-icon>
+      <span>Email Me</span>
+      <!-- material design icon lookup automatically -->
+    </v-btn>
+    <v-btn fab depressed medium class="teal accent-4 white--text">
+      <v-icon>favorite</v-icon>
 
-
+    </v-btn>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+// import HelloWorld from "../components/HelloWorld";
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {
+  // components: {
+  //   HelloWorld
+  // }
+};
 </script>
