32388f76a1c32b37f0838ecd160c92adbc771a93
noklam <channoklam@yahoo.com.hk>
2019-03-17 16:11:01 +0800

    Button Done

--- hello-world/src/views/Button.vue
+++ hello-world/src/views/Button.vue
@@ -1,19 +1,36 @@
 <template>
-<div class="button">
-  <h1 class="display-2">This is a page of a Fake Data Scientist</h1>
- <v-btn class="pink white--text">Click me la</v-btn>
- <v-btn depressed color="pink">Click me</v-btn>
-<v-btn flat color="pink">Click me</v-btn>
-</div>
- 
+  <div class="button">
+    <h1 class="display-2">This is a page of a Fake Data Scientist</h1>
+    <v-btn class="pink white--text">Click me la</v-btn>
+    <v-btn depressed color="pink">Click me</v-btn>
+    <v-btn flat color="pink">Click me</v-btn>
 
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>Email Me</span>
+      <!-- material design icon lookup automatically -->
+    </v-btn>
 
+    <v-btn depressed class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>Email Me</span>
+      <!-- material design icon lookup automatically -->
+    </v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon right large>email</v-icon>
+      <span>Email Me</span>
+      <!-- material design icon lookup automatically -->
+    </v-btn>
+
+    <v-btn fab depressed small class="pink white--text">
+      <v-icon>favorite</v-icon>
+      
+      <!-- fab to make the button become a circle -->
+    </v-btn>
+  </div>
 </template>
 
 <script>
-
-
-  export default {
- 
-  }
+export default {};
 </script>
