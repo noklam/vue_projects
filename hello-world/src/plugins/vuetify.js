8f52f74b4695ad667f2f5d06f5799904e4234ddf
noklam <channoklam@yahoo.com.hk>
2019-03-17 17:17:01 +0800

    drawer color now use theme instead hard code

--- hello-world/src/plugins/vuetify.js
+++ hello-world/src/plugins/vuetify.js
@@ -4,4 +4,9 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
-})
+  theme: {
+    primary: '#E0E0E0', // #E53935
+    secondary:'#80CBC4', // #FFCDD2
+    info: '#ffaa2c',
+    error: '#f83e70' // #3F51B5
+}})
