faa21607f53ecdf9b484e0cb0e35646dee622dec
noklam <channoklam@yahoo.com.hk>
2019-03-17 15:40:38 +0800

    Add Router

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import Trend from 'vuetrend';
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import Trend from "vuetrend";
+import router from './router'
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(Trend);
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
 
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
