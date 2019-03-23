1c608ccac925d354f81dfdff0ae02bd9c7984126
noklam <channoklam@yahoo.com.hk>
2019-03-23 14:02:31 +0800

    add cards for project dashboard

--- hello-world/src/components/Navbar.vue
+++ hello-world/src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Fake</span>
-        <span>Dashboard</span>
+        <span>Data Scientist</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat color="grey">
