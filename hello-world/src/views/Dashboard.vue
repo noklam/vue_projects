91d4559c7dbf6f99862aa0ffccd729ba7a9d5fa9
noklam <channoklam@yahoo.com.hk>
2019-03-23 13:54:28 +0800

    Grid System

--- hello-world/src/views/Dashboard.vue
+++ hello-world/src/views/Dashboard.vue
@@ -1,9 +1,39 @@
 <template>
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
-    <v-container class="my-5">content</v-container>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <!-- To make block start on new line if space is full -->
+        <v-flex xs12 md6>
+          <!--Let element become dynamic -->
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
+
 
 <script>
 export default {};
